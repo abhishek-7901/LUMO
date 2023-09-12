28ca971cd59a50cbd7242993b6a5e1ea533d7763
Ayush Paul <ayushpaul2002@gmail.com>
2023-09-12 19:52:50 +0530

    User Registration Backend

---
+++ src/main/java/com/wellsfargo/luma/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.wellsfargo.luma.repository;
+
+import com.wellsfargo.luma.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
