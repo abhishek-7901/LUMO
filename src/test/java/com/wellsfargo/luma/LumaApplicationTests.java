875ce7209cfbba9be6c23d0d306575fcd03467d6
Ayush Paul <ayushpaul2002@gmail.com>
2023-09-12 19:42:48 +0530

    Initial Backend Files

---
+++ src/test/java/com/wellsfargo/luma/LumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.luma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
