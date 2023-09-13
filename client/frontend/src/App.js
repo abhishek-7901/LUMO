206006379ea18ec5c7e5d389960707664d055b36
prabhat n <prabhat.namdharani@gmail.com>
2023-09-13 19:50:32 +0530

    Login logic implemented

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -2,23 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import Register from './components/Register'
-import LoginPage from './components/LoginPage';
 import Dashboard from './components/Dashboard';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 function App() {
+  // const [token, setToken] = useState()
+  // if (!token) {
+  //   return <LoginPage setToken={setToken} />
+  // }
   return (
     <Router>
       <div className="App">
-      
 
-          <Navbar></Navbar>
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-      
+
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+
       </div>
     </Router>
   );
--- client/frontend/src/components/Login.js
+++ client/frontend/src/components/Login.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import '../styles/Login.css'
+import { useState } from 'react'
 const Login = () => {
+  const [employeeId, setEmployeeId] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = async e=> {
+    e.preventDefault()
+    const user = {employeeId, password}
+    const response = await fetch('http://localhost:9191/employee/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    const data = await response.json()
+    console.log(data)
+  }
   return (
     <div>
       {/* Login Card */}
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Login</h5>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <label for="Username">Username</label>
-           <input type="text" name="username" />
-            <br></br>
-            <label for="Password">Password</label>
-              <input type="password" name="password" />
-            <br></br>
-            <input type="submit" value="Login" />
+              <label for="employeeId">employeeId</label>
+              <input type="text" name="employeeId" onChange={e=> setEmployeeId(e.target.value)} />
+              <br></br>
+              <label for="Password">Password</label>
+              <input type="password" name="password" onChange={e=> setPassword(e.target.value)} />
+              <br></br>
+              <input type="submit" value="Login" />
             </div>
           </form>
         </div>
