3b88b2ac20a077b89e350dec37b50b2105d69a92
Ayush Paul <ayushpaul2002@gmail.com>
2023-09-18 00:31:36 +0530

    Implemented /employee/applyLoan

--- src/main/java/com/wellsfargo/luma/controller/EmployeeController.java
+++ src/main/java/com/wellsfargo/luma/controller/EmployeeController.java
@@ -3,20 +3,22 @@ package com.wellsfargo.luma.controller;
 import com.wellsfargo.luma.model.Employee;
 import com.wellsfargo.luma.dto.LoginRequest;
 import com.wellsfargo.luma.dto.LoginResponse;
-import com.wellsfargo.luma.service.EmployeeService;
-import com.wellsfargo.luma.service.JwtService;
+import com.wellsfargo.luma.model.IssuedCard;
+import com.wellsfargo.luma.model.Item;
+import com.wellsfargo.luma.model.Loan;
+import com.wellsfargo.luma.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/employee")
@@ -32,6 +34,15 @@ public class EmployeeController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private LoanService loanService;
+
+    @Autowired
+    private ItemService itemService;
+
+    @Autowired
+    private IssuedCardService issuedCardService;
 
     @PostMapping("/new")
     public Map<String,Object> addEmployee(@RequestBody Employee employee){
@@ -68,6 +79,75 @@ public class EmployeeController {
 
 //        map.put("employee",employeeService.employeeLogin(authRequest));
 //        return map;
+
+    }
+
+    @PostMapping("/applyLoan")
+    public ResponseEntity<Map<String,Object>> addItem(@RequestBody Item item, @RequestHeader("Authorization") String authHeader){
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            String token = authHeader.substring(7);
+            if(token == null)
+            {
+                map.put("success" , false);
+                map.put("message","Error Fetching User. No User Found");
+                return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+            }
+            String name = jwtService.extractUsername(token);
+            Optional<Employee> employee = employeeService.findByName(name);
+            if(Objects.equals("EMP",employee.get().getRole())){
+
+                // Find a Loan Card which have the same type as the type of requested Item and is also not availed , i.e, status == false
+                // Mark both the Item and Loan as Availed, i.e , change the status of both to true;
+                // Make a new entry in the relation table
+                List<Loan> unavailedLoans = loanService.findUnavailedLoans(item.getCategory(),false);
+
+                if(unavailedLoans.isEmpty()){
+                    map.put("success" , false);
+                    map.put("Reason","No Unavailed Loan Cards Found");
+
+                    return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+                int markLoanAvailed = loanService.changeLoanStatus(unavailedLoans.get(0).getLoanId(),true);
+
+                if(markLoanAvailed<=0){
+                    map.put("success" , false);
+                    map.put("Reason","Error Changing Loan Status");
+
+                    return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+
+                int markItemAvailed = itemService.changeItemStatus(item.getItemId(),true);
+
+                if(markItemAvailed<=0){
+                    map.put("success" , false);
+                    map.put("Reason","Error Changing Item Status");
+
+                    return new ResponseEntity<>(map,HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+                IssuedCard issuedCard = new IssuedCard();
+                issuedCard.setEmpId(employee.get().getEmployeeId());
+                issuedCard.setItemId(item.getItemId());
+                issuedCard.setLoanId(unavailedLoans.get(0).getLoanId());
+                IssuedCard newCard = issuedCardService.addCard(issuedCard);
+
+                map.put("success",true);
+                map.put("CardDetails",newCard);
+
+                return new ResponseEntity<>(map,HttpStatus.CREATED);
+
+            }
+            else {
+                map.put("success", false);
+                map.put("Reason", "Not Authorized USER");
+                return new ResponseEntity<>(map, HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception e){
+            log.info(e.getStackTrace().toString());
+            map.put("success",false);
+            map.put("Reason","Check Credentials");
+            return ResponseEntity.internalServerError().body(map);
+        }
 
     }
 
