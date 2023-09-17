46177b21642e5e5e9879b75034d13c24537341bd
Ayush Paul <ayushpaul2002@gmail.com>
2023-09-18 00:29:23 +0530

    Implemented Item Model

---
+++ src/main/java/com/wellsfargo/luma/repository/ItemRepository.java
@@ -0,0 +1,23 @@
+package com.wellsfargo.luma.repository;
+
+import com.wellsfargo.luma.model.Item;
+import com.wellsfargo.luma.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    public Item findItemByItemId(String Id);
+
+    @Modifying
+    @Query("UPDATE Item u SET u.status = ?2 WHERE u.itemId = ?1")
+    public int changeItemStatus(String Id, Boolean status);
+
+
+
+}
