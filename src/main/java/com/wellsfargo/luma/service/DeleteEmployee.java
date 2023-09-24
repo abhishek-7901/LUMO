a5ad161c26e97472108f3d865d3d103a203a4a02
cs19b024 <cs19b024@smail.iitm.ac.in>
2023-09-24 20:30:12 +0530

    delete User

--- src/main/java/com/wellsfargo/luma/service/DeleteEmployee.java
+++
@@ -1,4 +0,0 @@
-package com.wellsfargo.luma.service;
-
-public class DeleteEmployee {
-}
---
+++ src/main/java/com/wellsfargo/luma/service/DeleteEmployeeService.java
@@ -0,0 +1,33 @@
+package com.wellsfargo.luma.service;
+
+
+import com.wellsfargo.luma.model.IssuedCard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DeleteEmployee {
+    @Autowired
+    IssuedCardService issuedCardService ;
+
+    @Autowired
+    LoanService loanService ;
+
+    @Autowired
+    ItemService itemService ;
+
+    public void deleteIssueCards(Long empId){
+        List<IssuedCard> issuedCardList = issuedCardService.findIssuedCardByEmpId(empId);
+
+        issuedCardList.forEach((card) -> {
+            loanService.changeLoanStatus(card.getLoanId(), false) ;
+            itemService.changeItemStatus(card.getItemId(), false) ;
+            issuedCardService.deleteById(card.getIssueId());
+        });
+        //now we have to delete the issuecards themselves.
+
+    }
+
+}
