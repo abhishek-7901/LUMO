440d3b0b23550c5d259ffd7519ddc7444ff82004
cs19b024 <cs19b024@smail.iitm.ac.in>
2023-09-24 23:27:35 +0530

    edit user

--- src/main/java/com/wellsfargo/luma/controller/AdminController.java
+++ src/main/java/com/wellsfargo/luma/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.wellsfargo.luma.model.Item;
 import com.wellsfargo.luma.model.Loan;
 import com.wellsfargo.luma.service.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -526,6 +527,50 @@ public class AdminController {
             }
         } catch (Exception e){
             log.info(e.getStackTrace().toString());
+            map.put("success",false);
+            map.put("Reason","Check Credentials");
+            return ResponseEntity.internalServerError().body(map);
+        }
+    }
+
+    @PutMapping("/editEmployee/{id}")
+    public ResponseEntity<Map<String, Object>> editUser(@PathVariable(value = "id") Long eId,
+                                                        @RequestBody Employee newEmp, @RequestHeader("Authorization") String authHeader){
+        Map<String, Object> map = new HashMap<String, Object>() ;
+
+        try {
+            String token = authHeader.substring(7);
+            if (token == null) {
+                map.put("success", false);
+                map.put("message", "Error fetching user. No user found");
+                return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+            }
+            String name = jwtService.extractUsername(token);
+            Optional<Employee> employee = employeeService.findByName(name);
+
+            if (Objects.equals("ADMIN", employee.get().getRole())) {
+
+                Optional<Employee> oldEmp = employeeService.findById(eId);
+
+                if (oldEmp.isPresent()) {
+                    newEmp.setEmployeeId(oldEmp.get().getEmployeeId());
+                    Employee emp = employeeService.addEmployee(newEmp, "EMP") ;
+                    map.put("employee", emp) ;
+                    map.put("success", true) ;
+                    return new ResponseEntity<>(map, HttpStatus.OK) ;
+                } else {
+                    map.put("Employee Id doesn't exist", false);
+                    return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+                }
+            } else {
+                map.put("success", false) ;
+                map.put("You are not authorized", false);
+                return new ResponseEntity<>(map, HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception e){
+            log.warn("ERROR IS BELOW");
+            e.printStackTrace();
+//            log.error(e.getStackTrace().toString());
             map.put("success",false);
             map.put("Reason","Check Credentials");
             return ResponseEntity.internalServerError().body(map);
