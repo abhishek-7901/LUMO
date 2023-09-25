03497dbf2b88c5e91dc3f7df70af356675b20b21
prabhat n <prabhat.namdharani@gmail.com>
2023-09-25 16:17:18 +0530

    viewLoan and viewItem working for user

--- client/frontend/src/components/User/ViewItem.js
+++ client/frontend/src/components/User/ViewItem.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../../styles/ViewLoan.css'
 
 const ViewItem = () => {
+
+  const [items,setItems] = useState([]);
+
+ useEffect(() => {
+  getItemData()
+  }, [])
+
+
+  function getItemData() {
+    fetch('http://localhost:9191/employee/viewItems', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}` || ''
+        }
+    }).then(response => {
+        //console.log(response)
+        return response.json()
+    }).then(data => {
+        console.log(data)
+        setItems(data["ItemList"])
+        // console.log(customers)
+    })
+}
   return (
-    <div>ViewItem</div>
+    <div>
+      <br/>
+
+      <h2 className="text-success">Loan Management Application</h2>
+      <br/>
+      <h3>Items Availed</h3>
+      <br/>
+        <div className="row justify-content-center"></div>
+      <br/>
+      <div className="row justify-content-center">
+        <table className="table table-success w-auto">
+          <thead>
+            <tr>
+              <th>Item id </th>
+              <th>Category </th>
+              <th>Description</th>
+              <th>Make</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => {
+              return(
+                <tr>
+                  <td>{item.itemId}</td>
+                  <td>{item.category}</td>
+                  <td>{item.description}</td>
+                  <td>{item.make}</td>
+                  <td>{item.value}</td>
+
+                </tr>
+              )
+            })}
+            
+          </tbody>
+        </table>
+    </div>
+    </div>
+    
   )
 }
 
